0342d0bb73345d360dd235d7169443c41a0a2e81
Paul Harrison <pharrison@mythtv.org>
2019-11-23 17:54:30 +0000

    Don't treat failing to open the MythTV database as fatal
    allows the user to enter the correct credentials on the settings page

--- mythfrontend_qml/main.cpp
+++ mythfrontend_qml/main.cpp
@@ -65,12 +65,16 @@ int main(int argc, char *argv[])
 
     gContext->init();
 
-    // attempt to connect to the MythTV database
+    // attempt to connect to the MythTV database using our stored credentials
     if (!gContext->initMythDB())
     {
-        // try to get the DB credentials from the config.xml
+        // failed so try to get the DB credentials from the config.xml
         if (!gContext->loadMythDBSettings())
-            return 1;
+        {
+            // failed to open MythTV database using stored/default credentials or from the mythtv config.xml file
+            gContext->m_logger->error(Verbose::GENERAL, "Failed to open the MythTV database - Please make sure the "
+                                                        "Mysql settings are correct on the Myth Backend settings page");
+        }
     }
 
     // these are frontend only
