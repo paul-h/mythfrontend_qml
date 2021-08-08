fe8c06eca09c87d14b9a681808450b0ae02079cc
Paul Harrison <paul@mythqml.net>
2021-08-08 12:12:18 +0100

    MDK: add a script to download and install the MDK_SDK libraries
    The script will download the full SDK and extract the libraries for one of
    the supported architectures (amd64, arm64, armhf) and install them where
    MythQML can find them and dynamically load them if needed.
    
    EXAMPLES
    
    # install amd64 libraries (default if no option is given)
    sudo /usr/share/mythtv/qml/Scripts/installMDK.sh
    
    # install arm64 libraries
    sudo /usr/share/mythtv/qml/Scripts/installMDK.sh arm64
    
    # install armhf libraries
    sudo /usr/share/mythtv/qml/Scripts/installMDK.sh armhf
    
    (cherry picked from commit ae60ef00f54b8c73f5b09229783f093418ee2820)

---
+++ Scripts/installMDK.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# mdk-sdk version to install
+MDK_VERSION="v0.12.0"
+
+# default architecture
+ARCH="amd64"
+
+set -eu -o pipefail # fail on error , debug all lines
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+if [ "$#" -eq 1 ]; then
+    ARCH=$1
+fi
+
+if [ "$ARCH" != "amd64"  ] && [ "$ARCH" != "arm64" ] && [ "$ARCH" != "armhf" ]; then
+    echo "Invalid architecture ($ARCH) should be one of amd64, arm64 or armhf"
+    exit 1
+fi
+
+echo Installing the MDK-SDK libraries for $ARCH
+
+echo Downloading the MDK-SDK Version $MDK_VERSION
+wget -q -O /tmp/mdk-sdk-linux.tar.xz https://github.com/wang-bin/mdk-sdk/releases/download/$MDK_VERSION/mdk-sdk-linux.tar.xz
+
+echo Extracting the archive
+tar -x -f /tmp/mdk-sdk-linux.tar.xz -C /tmp
+
+echo Making sure the install directory exists and is empty
+mkdir -p "/lib/mythqml"
+rm -rf "/lib/mythqml/*"
+
+echo Installing the MDK-SDK libraries
+cp -rf /tmp/mdk-sdk/lib/$ARCH/* /lib/mythqml/
+
+echo Cleaning up
+rm -rf /tmp/mdk-sdk
+rm /tmp/mdk-sdk-linux.tar.xz
+
+echo DONE
