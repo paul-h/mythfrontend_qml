55b9435f45c4365d581d9fd8da60f503e9a81ffd
Paul Harrison <paul@mythqml.net>
2023-05-01 15:20:29 +0100

    SqlQueryModel: make it a registered QML component

--- mythfrontend_qml/sqlquerymodel.cpp
+++ mythfrontend_qml/sqlquerymodel.cpp
@@ -1,10 +1,46 @@
-#include "sqlquerymodel.h"
 #include <QSqlRecord>
 #include <QSqlField>
+#include <QSqlQuery>
+#include <QDebug>
+
+#include "sqlquerymodel.h"
+#include "context.h"
 
 SqlQueryModel::SqlQueryModel(QObject *parent) :
 QSqlQueryModel(parent)
 {
+    m_useMythQMLDB = true;
+}
+
+void SqlQueryModel::setSql(const QString &sql)
+{
+    m_sql = sql;
+
+    clear();
+
+    if (m_useMythQMLDB)
+        setQuery(m_sql, gContext->m_mythQMLDB);
+    else
+        setQuery(m_sql, gContext->m_mythDB);
+
+    emit sqlChanged(sql);
+}
+
+QString SqlQueryModel::sql(void)
+{
+    return m_sql;
+}
+
+void SqlQueryModel::setUseMythQMLDB(bool useMythQMLDB)
+{
+    m_useMythQMLDB = useMythQMLDB;
+
+    emit useMythQMLDBChanged(useMythQMLDB);
+}
+
+bool SqlQueryModel::useMythQMLDB(void)
+{
+    return m_useMythQMLDB;
 }
 
 void SqlQueryModel::setQuery(const QString &query, const QSqlDatabase &db)
@@ -22,6 +58,7 @@ void SqlQueryModel::setQuery(const QSqlQuery &query)
 void SqlQueryModel::generateRoleNames()
 {
     m_roleNames.clear();
+
     for( int i = 0; i < record().count(); i ++)
     {
         m_roleNames.insert(Qt::UserRole + i + 1, record().fieldName(i).toUtf8());
@@ -44,4 +81,9 @@ QVariant SqlQueryModel::data(const QModelIndex &index, int role) const
     }
 
     return value;
+}
+
+void SqlQueryModel::reload()
+{
+    setSql(m_sql);
 }
